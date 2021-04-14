244f0aacce58cf83ba860feb3fb02bb7faa9912b
Ahmad Fadhel <ahmad.f.ameen@gmail.com>
2021-04-14 02:34:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+
+
+function submitData(name, email) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+      .then( function (response) {
+        return response.json()
+      } )
+      .then( function (json) {
+        document.body.innerHTML = json.id;
+      } )
+      .catch( function (err) {
+        alert("There was an Error. please try again later");
+        document.body.innerHTML = err.message
+      } )
+  } 
