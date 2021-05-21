bffc9733e6d2e6eb7b236eefffa57987c978979a
Omer Sabah <omerrj99@gmail.com>
2021-05-21 23:32:07 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Add your code here
+const submitData=( name, email )=> 
+  fetch( 'http://localhost:3000/users', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify( {
+        name,
+        email
+      } )
+    } )
+    .then( r  => r.json())
+    .then( d=>document.body.innerHTML = d[ "id" ])
+    .catch( e =>document.body.innerHTML = e.message)
